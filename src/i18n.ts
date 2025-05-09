8b22650e42b79a13c14b42acc9c1cfea0121e85e
danielbom <daniel.rodrigues.45@hotmail.com>
2025-05-09 15:40:32 -0300

    Update index.html

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,9 @@ import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const loadPath = `${import.meta.env.BASE_URL}locales/{{lng}}/{{ns}}.json`
+console.log(`loadPath = ${loadPath}`)
+
 i18n
   .use(HttpBackend) // load translations using http (default public/locales)
   .use(LanguageDetector) // detect user language
@@ -14,7 +17,7 @@ i18n
       escapeValue: false, // react already does escaping
     },
     backend: {
-      loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/{{ns}}.json`,
+      loadPath,
     },
   });
 
