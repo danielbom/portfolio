75b3aba315181920dfbe9380e46cc106924f702d
danielbom <daniel.rodrigues.45@hotmail.com>
2026-08-06 02:58:25 -0300

    Update ProjectAdventOfCode

--- src/routes/x/en/projects/ProjectAdventOfCode.svelte
+++ src/routes/x/en/projects/ProjectAdventOfCode.svelte
@@ -4,8 +4,8 @@
 	import aocImage from '$lib/images/projects/advent-of-code.jpeg';
 </script>
 
-<Project name="Advent of Code" image={aocImage} link="https://github.com/danielbom/advent-of-code">
+<Project name="Advent of Code (3 Years)" image={aocImage} link="https://github.com/danielbom/advent-of-code">
 	This repository gathers my solutions for the Advent of Code challenges, with the 2015 challenges
-	solved in Rust and the 2016 ones in Go. My goal is to explore the challenges of other years using
-	different languages, improving my skills and learning new concepts along the way.
+	solved in Rust, the 2016 ones in Go and the 2017 in Gleam. My goal is to explore the challenges of
+	other years using different languages, improving my skills and learning new concepts along the way.
 </Project>
--- src/routes/x/pt-br/projects/ProjectAdventOfCode.svelte
+++ src/routes/x/pt-br/projects/ProjectAdventOfCode.svelte
@@ -4,9 +4,9 @@
 	import aocImage from '$lib/images/projects/advent-of-code.jpeg';
 </script>
 
-<Project name="Advent of Code" image={aocImage} link="https://github.com/danielbom/advent-of-code">
+<Project name="Advent of Code (3 Anos)" image={aocImage} link="https://github.com/danielbom/advent-of-code">
 	Este repositório reúne minhas soluções para os desafios do Advent of Code, com os desafios de 2015
-	resolvidos em Rust e os de 2016 em Go. Meu objetivo é explorar os desafios dos outros anos
-	utilizando diferentes linguagens, aprimorando minhas habilidades e aprendendo novos conceitos ao
-	longo do caminho.
+	resolvidos em Rust, os de 2016 em Go e os de 2017 em Gleam. Meu objetivo é explorar os desafios dos
+	outros anos utilizando diferentes linguagens, aprimorando minhas habilidades e aprendendo novos 
+	conceitos ao longo do caminho.
 </Project>
